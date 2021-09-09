aeda7d5235ffca0fe798c3679527c21716224b12
Vazha Chiaberashvili <chiaberashv1@gmail.com>
2021-09-09 19:17:50 -0400

    Added handleValidationErrors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,12 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
 
 module.exports = router;
