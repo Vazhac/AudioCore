69d7e0bc9ea07981ff29efd08218bd3f1f3c3161
Vazha Chiaberashvili <chiaberashv1@gmail.com>
2021-09-18 21:04:58 -0400

    added EditCommentModal

---
+++ frontend/src/components/EditCommentModal/index.js
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import EditComment from './EditComment';
+import '../Navigation/Navigation.css'
+
+function EditCommentModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button id="edit-comment-button" onClick={() => setShowModal(true)}>
+        Edit Comment
+      </button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <EditComment />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default EditCommentModal;
